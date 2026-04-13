bc7c4f73593c102619da4a64fabb0297d7e52d45
Umer Rathore <omes000@gmail.com>
2026-04-12 23:27:49 -0500

    Implement loadVideo function for embedding YouTube
    Add a function to load a YouTube video into an element.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ window.addEventListener('scroll', () => {
   if (window.scrollY > 8) header.classList.add('is-scrolled');
   else header.classList.remove('is-scrolled');
 });
+function loadVideo(el) {
+  el.innerHTML = `
+    <iframe
+      src="https://www.youtube.com/embed/90rdcI61eIo?autoplay=1&controls=1&rel=0&modestbranding=1&playsinline=1"
+      title="MedCode Pro platform video"
+      allow="autoplay; encrypted-media; picture-in-picture"
+      allowfullscreen>
+    </iframe>
+  `;
+}
